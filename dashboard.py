ab5aad01780db8b8600e3f2508fb7e6637dd67c7
Francesco Witte <francesco.witte@dlr.de>
2023-11-06 13:39:22 +0100

    Add template for dashboard

---
+++ dashboard.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+from bokeh.plotting import figure
+
+
+
+def load_data() -> pd.DataFrame:
+
+    df = pd.read_csv("data/Mark_Ie_Daten.csv", index_col=0, header=0)
+    return df
+
+
+df = load_data()
+
+
+st.write(df)
+st.line_chart(df, x=None, y="unemployment")
+option = st.selectbox(
+    label="Parameter",
+    options=df.columns
+)
+
+p = figure(
+    title='unemployment over model iterations',
+    x_axis_label='model iteration',
+    y_axis_label='unemployment rate'
+)
+p.line(df.index, df[option], legend_label='Trend', line_width=2)
+st.bokeh_chart(p, use_container_width=True)
