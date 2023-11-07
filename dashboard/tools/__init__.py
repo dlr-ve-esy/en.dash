226dc0c73cb12f046b4f95247b0e39b507fc37bc
Fuchs <Benjamin.Fuchs@dlr.de>
2023-11-07 16:23:44 +0100

    fixes bug in recursion for updating the dict

--- dashboard/tools/__init__.py
+++ dashboard/tools/__init__.py
@@ -2,15 +2,26 @@ import streamlit as st
 import collections.abc
 
 
+def __update(org, up):
+    for k, v in up.items():
+        if isinstance(v, collections.abc.Mapping):
+            org[k] = __update(org.get(k, {}), v)
+        else:
+            org[k] = v
+    return org
+
+
 def update_options_with_defaults(options):
     defaults = {
         "backgroundColor": "#FFFFFF"
         if st.session_state["style"] != "dark"
         else "#0E1117",
+        "feature": {
+            "mark": {"show": "true"},
+            "dataView": {"show": "true", "readOnly": "false"},
+            "magicType": {"show": "true", "type": ["line", "bar"]},
+            "restore": {"show": "true"},
+            "saveAsImage": {"show": "true"},
+        },
     }
-    for k, v in defaults.items():
-        if isinstance(v, collections.abc.Mapping):
-            options[k] = update_options_with_defaults(options.get(k, {}), v)
-        else:
-            options[k] = v
-    return options
+    return __update(options, defaults)
