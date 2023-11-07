793596289eb998a8b544dfd25786ebaf611408e4
Fuchs <Benjamin.Fuchs@dlr.de>
2023-11-07 16:47:42 +0100

    adds logo to sidebar and switches between dark and light mode

--- dashboard/layout/sidebar.py
+++ dashboard/layout/sidebar.py
@@ -1,11 +1,21 @@
 import streamlit as st
+from PIL import Image
 
 
 def create():
-    st.write("this is the sidebar")
-    darkmode_enabled = st.toggle("enable dark mode")
-    with st.spinner(text="switching style ..."):
-        if darkmode_enabled:
-            st.session_state["style"] = "dark"
-        else:
-            st.session_state["style"] = "light"
+    if st.session_state["style"] == "dark":
+        logo = Image.open("data/column-chart-line-icon-black.png")
+        logo = logo.resize((150, 100))
+
+        st.image(logo)
+    else:
+        logo = Image.open("data/column-chart-line-icon-white.png")
+        logo = logo.resize((150, 100))
+
+        st.image(logo)
+    st.write("Dashboard")
+    darkmode_enabled = st.toggle("enable dark mode for plots")
+    if darkmode_enabled:
+        st.session_state["style"] = "dark"
+    else:
+        st.session_state["style"] = "light"
