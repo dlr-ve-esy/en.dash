0c99060cfefd6e50684a4752249c4499799a8afd
Josua Braun <josua.braun@dlr.de>
2023-11-09 15:06:38 +0100

    barplot classes to functions

--- dashboard/plots/barplots.py
+++ dashboard/plots/barplots.py
@@ -1,218 +1,161 @@
+from dashboard.tools import update_options_with_defaults, update_options_with_user_overrides
 
-
-class BarplotSimple():
-
-    def __init__(self, data, metadata=None):
-        self.data = data
-        self.metadata = metadata
-
-    def build_options(self):
-        x_col = self.data.columns[0]
-        y_col = self.data.columns[1]
-
-        x = list(self.data[x_col])
-        y = list(self.data[y_col])
-
-        options = {
-            'tooltip': {
-                'trigger': 'axis',
-                'axisPointer': {
+def _default_barplot_options():
+    options = {
+        'tooltip': {
+            'trigger': 'axis',
+            'axisPointer': {
                 'type': 'shadow'
-                }
-            },
-            'xAxis': {
-                'type': 'category',
-                'data': x
-            },
-            'yAxis': {
-                'type': 'value',
-                # 'name': f'{self.metadata[y_col]["label"]} in {self.metadata[y_col]["unit"]}',
-                'nameLocation': 'middle',
-                'nameGap': 50
-            },
-            'series': [
-                {
-                'data': y,
-                'type': 'bar'
-                }
-            ]
+            }
+        },
+        'xAxis': {
+            'type': 'category',
+        },
+        'yAxis': {
+            'type': 'value',
+            'nameLocation': 'middle',
+            'nameGap': 50
         }
+    }
+    return options
 
-        return options
+
+def _default_barplot_series_options():
+    options = {
+        'type': 'bar',
+        'barGap': 0.1,
+        'emphasis': {
+            'focus': 'series'
+        },
+    }
+    return options
+
+
+def barplot_simple(data, metadata=None):
+    x_col = data.columns[0]
+    y_col = data.columns[1]
+
+    x = list(data[x_col].astype(str))
+    y = list(data[y_col])
+
+    options = {
+        'xAxis': {
+            'data': x
+        },
+        'yAxis': {
+            'type': 'value',
+        },
+        'series': [
+            {
+            'data': y,
+            'type': 'bar'
+            }
+        ]
+    }
+
+    options = update_options_with_user_overrides(_default_barplot_options(), options)
+    return options
     
 
-class BarplotGrouped():
+def barplot_grouped(data, metadata=None):
+    x_col = data.columns[0]
+    y_col = data.columns[1]
+    g_col = data.columns[2]
 
-    def __init__(self, data, metadata=None):
-        self.data = data
-        self.metadata = metadata
+    x = list(data[x_col].astype(str).unique())
 
-    def build_options(self):
-        x_col = self.data.columns[0]
-        y_col = self.data.columns[1]
-        g_col = self.data.columns[2]
-
-        x = list(self.data[x_col].astype(str).unique())
-
-        series_list = list()
-        groups = list(self.data[g_col].unique())
-        for selection, s in self.data.groupby(g_col):
-            y = list(s[y_col])
-            d = {
-                'name': selection,
-                'type': 'bar',
-                'barGap': '0',
-                'label': {
-                    'show': True
-                },
-                'emphasis': {
-                    'focus': 'series'
-                },
-                'data': y
-            }
-            series_list.append(d)
-        
-        options = {
-            'tooltip': {
-                'trigger': 'axis',
-                'axisPointer': {
-                'type': 'shadow'
-                }
-            },
-            'legend': {
-                'data': groups
-            },
-            'xAxis': {
-                'type': 'category',
-                'data': x
-            },
-            'yAxis': {
-                'type': 'value',
-                # 'name': f'{self.metadata[y_col]["label"]} in {self.metadata[y_col]["unit"]}',
-                'nameLocation': 'middle',
-                'nameGap': 50
-            },
-            'series': series_list
+    series_list = list()
+    groups = list(data[g_col].astype(str).unique())
+    for selection, s in data.groupby(g_col):
+        y = list(s[y_col])
+        d = {
+            'name': selection,
+            'data': y
         }
+        d.update(_default_barplot_series_options())
+        series_list.append(d)
+    
+    options = {
+        'legend': {
+            'data': groups
+        },
+        'xAxis': {
+            # 'data': x + x,
+            # 'data': groups
+            'data': [groups, groups]
+        },
+        'series': series_list
+    }
 
-        return options
+    options = update_options_with_user_overrides(_default_barplot_options(), options)
+    return options
     
 
-class BarplotStacked():
+def barplot_stacked(self, data, metadata=None):
+    x_col = data.columns[0]
+    y_col = data.columns[1]
+    s_col = data.columns[2]
 
-    def __init__(self, data, metadata=None):
-        self.data = data
-        self.metadata = metadata
-
-    def build_options(self):
-        x_col = self.data.columns[0]
-        y_col = self.data.columns[1]
-        s_col = self.data.columns[2]
-
-        series_list = list()
-        x = list(self.data.astype(str)[x_col].unique())
-        stacks = list(self.data[s_col].unique())
-        for selection, s in self.data.groupby(s_col):
-            y = list(s[y_col])
-            d = {
-                'name': selection,
-                'type': 'bar',
-                'barGap': '0',
-                'stack': 'total',
-                'label': {
-                    'show': True
-                },
-                'emphasis': {
-                    'focus': 'series'
-                },
-                'data': y
-            }
-            series_list.append(d)
-        
-        options = {
-            'tooltip': {
-                'trigger': 'axis',
-                'axisPointer': {
-                'type': 'shadow'
-                }
-            },
-            'legend': {
-                'data': stacks
-            },
-            'xAxis': {
-                'type': 'category',
-                'data': x
-            },
-            'yAxis': {
-                'type': 'value',
-                # 'name': f'{self.metadata[y_col]["label"]} in {self.metadata[y_col]["unit"]}',
-                'nameLocation': 'middle',
-                'nameGap': 50
-            },
-            'series': series_list
+    series_list = list()
+    x = list(data.astype(str)[x_col].unique())
+    stacks = list(data[s_col].astype(str).unique())
+    for selection, s in data.groupby(s_col):
+        y = list(s[y_col])
+        d = {
+            'name': selection,
+            'stack': 'total',
+            'data': y
         }
-
-        return options
+        d.update(_default_barplot_series_options())
+        series_list.append(d)
+    
+    options = {
+        'legend': {
+            'data': stacks
+        },
+        'xAxis': {
+            'data': x
+        },
+        'series': series_list
+    }
+    
+    options = update_options_with_user_overrides(_default_barplot_options(), options)
+    return options
     
 
-class BarplotGroupedStacked():
+def barplot_grouped_stacked(data, metadata):
+    x_col = data.columns[0]
+    y_col = data.columns[1]
+    g_col = data.columns[2]
+    s_col = data.columns[3]
 
-    def __init__(self, data, metadata=None):
-        self.data = data
-        self.metadata = metadata
-
-    def build_options(self):
-        x_col = self.data.columns[0]
-        y_col = self.data.columns[1]
-        g_col = self.data.columns[2]
-        s_col = self.data.columns[3]
-
-        x = list(self.data[x_col].astype(str).unique())
-        stacks = list(self.data[s_col].unique())
-        series_list = list()
-        for selection, s in self.data.groupby([g_col, s_col]):
-            group_name = selection[0]
-            stack_name = selection[1]
-            y = list(s[y_col])
-            d = {
-                'name': stack_name,
-                'type': 'bar',
-                'barGap': '0',
-                'stack': f'{group_name}',
-                'label': {
-                    'show': True
-                },
-                'emphasis': {
-                    'focus': 'series'
-                },
-                'data': y
-            }
-            series_list.append(d)
-        
-        options = {
-            'tooltip': {
-                'trigger': 'axis',
-                'axisPointer': {
-                'type': 'shadow'
-                }
-            },
-            'legend': {
-                'data': stacks
-            },
-            'xAxis': {
-                'type': 'category',
-                'data': x
-            },
-            'yAxis': {
-                'type': 'value',
-                # 'name': f'{self.metadata[y_col]["label"]} in {self.metadata[y_col]["unit"]}',
-                'nameLocation': 'middle',
-                'nameGap': 50
-            },
-            'series': series_list
+    x = list(data[x_col].astype(str).unique())
+    stacks = list(data[s_col].astype(str).unique())
+    series_list = list()
+    for selection, s in data.groupby([g_col, s_col]):
+        group_name = selection[0]
+        stack_name = selection[1]
+        y = list(s[y_col].astype(str))
+        d = {
+            'name': stack_name,
+            'stack': f'{group_name}',
+            'data': y
         }
+        d.update(_default_barplot_series_options())
+        series_list.append(d)
+    
+    options = {
+        'legend': {
+            'data': stacks
+        },
+        'xAxis': {
+            'data': x
+        },
+        'series': series_list
+    }
 
-        return options
+    options = update_options_with_user_overrides(_default_barplot_options(), options)
+    return options
 
 
 
--- dashboard/tabs/tab1.py
+++ dashboard/tabs/tab1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from dashboard.plots.barplots import BarplotSimple, BarplotGrouped, BarplotStacked, BarplotGroupedStacked
+from dashboard.plots.barplots import barplot_simple, barplot_grouped, barplot_stacked, barplot_grouped_stacked
 from dashboard.tools import update_options_with_defaults, update_options_with_user_overrides
 from streamlit_echarts import st_echarts
 from streamlit_extras.chart_container import chart_container
@@ -8,10 +8,7 @@ from streamlit_extras.chart_container import chart_container
 import pandas as pd
 
 
-
-def create(data, metadata):
-    print(data)
-    print(metadata)
+def create(data, metadata, cfg):
 
     def add_filters(config, data):
         filters_value = dict()
@@ -26,76 +23,90 @@ def create(data, metadata):
     navbar, plotarea = st.columns([0.2, 0.8])
     with navbar:
         st.write("Plot configuration")
-        plot_type = st.selectbox(
-            label='Plot type',
-            options=['simple', 'grouped', 'stacked', 'grouped & stacked'],
+
+        y_value = 'InstalledPower'
+        foptions_x = list(data.reset_index(drop=True).columns)
+        foptions_x = [item for item in foptions_x if item != y_value]
+        fval_x = st.selectbox(
+            label='x-axis',
+            options=foptions_x
         )
 
+        foptions_g = [item for item in foptions_x if item != fval_x]
+        foptions_g_all = ['None'] + foptions_g
+        fval_g = st.selectbox(
+            label='grouped',
+            options=foptions_g_all
+        )
+
+        foptions_s = [item for item in foptions_g if item != fval_g]
+        foptions_s_all = ['None'] + foptions_s
+        fval_s = st.selectbox(
+            label='stacked',
+            options=foptions_s_all
+        )
+
+        f_options_rest = [item for item in foptions_s if item != fval_s]
+
+        if fval_g == 'None' and fval_s == 'None':
+            plot_type = 'simple'
+        elif fval_g != 'None' and fval_s == 'None':
+            plot_type = 'grouped'
+        elif fval_g == 'None' and fval_s != 'None':
+            plot_type = 'stacked'
+        elif fval_g != 'None' and fval_s != 'None':
+            plot_type = 'grouped & stacked'
+
+        config_plot = {
+            'x': fval_x,
+            'y': y_value,
+            'filters': f_options_rest,
+            'groups': fval_g,
+            'stacks': fval_s,
+        }
+
+        if config_plot['filters']:
+            st.write("Data filters")
+
         if plot_type == 'simple':
-            config_plot = {
-                'x': 'Year',
-                'y': 'InstalledPower',
-                'filters': ['Technology', 'Region']
-            }
             data_plot = data.reset_index()
             filters_value = add_filters(config_plot, data_plot)
             for filter, value in filters_value.items():
                 con = data_plot[filter] == value
                 data_plot = data_plot.loc[con]
             data_plot = data_plot[[config_plot['x'], config_plot['y']]]
-            plot_class = BarplotSimple
+            plot_class = barplot_simple
 
         elif plot_type == 'grouped':
-            config_plot = {
-                'filters': ['Region'],
-                'x': 'Year',
-                'y': 'InstalledPower',
-                'groups': 'Technology'
-            }
             data_plot = data.reset_index()
             filters_value = add_filters(config_plot, data_plot)
             for filter, value in filters_value.items():
                 con = data_plot[filter] == value
                 data_plot = data_plot.loc[con]
             data_plot = data_plot[[config_plot['x'], config_plot['y'], config_plot['groups']]]
-            plot_class = BarplotGrouped
+            plot_class = barplot_grouped
 
         elif plot_type == 'stacked':
-            config_plot = {
-                'filters': ['Region'],
-                'x': 'Year',
-                'y': 'InstalledPower',
-                'stacks': 'Technology'
-            }
             data_plot = data.reset_index()
             filters_value = add_filters(config_plot, data_plot)
             for filter, value in filters_value.items():
                 con = data_plot[filter] == value
                 data_plot = data_plot.loc[con]
             data_plot = data_plot[[config_plot['x'], config_plot['y'], config_plot['stacks']]]
-            plot_class = BarplotStacked
+            plot_class = barplot_stacked
 
         elif plot_type == 'grouped & stacked':
-            config_plot = {
-                'x': 'Year',
-                'y': 'InstalledPower',
-                'groups': 'Region',
-                'stacks': 'Technology'
-            }
             data_plot = data.reset_index()
             filters_value = add_filters(config_plot, data_plot)
             for filter, value in filters_value.items():
                 con = data_plot[filter] == value
                 data_plot = data_plot.loc[con]
             data_plot = data_plot[[config_plot['x'], config_plot['y'], config_plot['groups'], config_plot['stacks']]]
-            plot_class = BarplotGroupedStacked
-        else:
-            options = {}
+            plot_class = barplot_grouped_stacked
 
     with plotarea:
         with chart_container(pd.DataFrame(data)):
-            plot_obj = plot_class(data_plot, metadata)
-            options = plot_obj.build_options()
+            options = plot_class(data_plot, metadata)
             options_update = {
                 'yAxis': {
                     'name': f'{metadata[config_plot["y"]]["label"]} in {metadata[config_plot["y"]]["unit"]}'
@@ -103,6 +114,21 @@ def create(data, metadata):
             }
             options = update_options_with_user_overrides(options, options_update)
             options = update_options_with_defaults(options)
+
+            for series_item in options['series']:
+                insert = {
+                    'label': {
+                        'show': False,
+                        'position': 'top',
+                        'rotate': 90,
+                        'formatter': '{c}  {name|{a}}',
+                        'fontSize': 16,
+                        'rich': {
+                            'name': {}
+                        }
+                    }
+                }
+                series_item.update(insert)
 
             st_echarts(options=options, height="400px")
  
