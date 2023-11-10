7d17366149e4daa7097615a7a51831bc027aa129
Josua Braun <josua.braun@dlr.de>
2023-11-10 10:11:38 +0100

    default axis options added to scatterplot

--- dashboard/plots/scatterplots.py
+++ dashboard/plots/scatterplots.py
@@ -1,3 +1,20 @@
+from dashboard.tools import update_options_with_user_overrides
+
+
+def _default_scatterplot_options():
+    options = {
+        'yAxis': {
+            'nameLocation': 'middle',
+            'nameGap': 50,
+        },
+        'xAxis': {
+            'nameLocation': 'middle',
+            'nameGap': 50,
+        }
+    }
+    return options
+
+
 def scatterplot(data, metadata):
     options = {
         "xAxis": {},
@@ -8,4 +25,6 @@ def scatterplot(data, metadata):
             "type": "scatter",
         },
     }
+
+    options = update_options_with_user_overrides(_default_scatterplot_options(), options)
     return options
