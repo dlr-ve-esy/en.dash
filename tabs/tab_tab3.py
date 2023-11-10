5801b6fbb6859a2831cb272d3601d5ffc7190990
Josua Braun <josua.braun@dlr.de>
2023-11-10 10:15:32 +0100

    functionable scatterplot

--- tabs/tab_tab3.py
+++ tabs/tab_tab3.py
@@ -9,40 +9,63 @@ import numpy as np
 from streamlit_extras.chart_container import chart_container
 import pandas as pd
 
+from dashboard.plots.scatterplots import scatterplot
+
 
 def create(data, metadata, plots_cfg):
     # strompreis vs. last (awardedpower)
-    data = data["SingleKey"]
-    metadata = metadata["SingleKey"]
 
-    data = data[["StoredEnergy_Austria", "StoredEnergy_Germany"]].values
+    data = data['Line/TwoKey']
+    metadata = metadata['Line/TwoKey']
 
-    st.subheader("test plots")
+    data = data.reset_index()
+
     navbar, plotarea = st.columns([0.2, 0.8])
 
     with navbar:
-        st.write("this is a nav bar")
+        selected_region = st.selectbox(
+            label='Region Selection',
+            options=data['Region'].unique()
+        )
+
+        xaxis_options = ['ElectricityPrice', 'AwardedEnergy', 'Shortage', 'StoredEnergy']
+        selected_x = st.selectbox(
+            label='X-Axis',
+            options=xaxis_options
+        )
+
+        yaxis_options = [item for item in xaxis_options if item != selected_x]
+        selected_y = st.selectbox(
+            label='Y-Axis',
+            options=yaxis_options
+        )
+
+        data = data.loc[data['Region'] == selected_region][[selected_x, selected_y]].values
+        # data = data.set_index(selected_x, drop=True)
 
     with plotarea:
-        chart_container(pd.DataFrame(data), tabs=["Export"])
-        options = {
-            "xAxis": {},
-            "yAxis": {},
-            "series": {
-                "symbolSize": 10,
-                "data": data.tolist(),
-                "type": "scatter",
-            },
-        }
+        options = scatterplot(data, metadata)
+
         user = delete_barred_user_overrides(
-            plots_cfg["experimental_plot"],
+            plots_cfg["scatterplot"],
             {
                 "xAxis": None,
                 "yAxis": None,
             },
         )
+
         options = update_options_with_user_overrides(options, user)
         options = update_options_with_defaults(options)
+
+        options_update = {
+                'yAxis': {
+                    'name': f'{metadata[selected_y]["label"]} in {metadata[selected_y]["unit"]}'
+                },
+                'xAxis': {
+                    'name': f'{metadata[selected_x]["label"]} in {metadata[selected_x]["unit"]}'
+                }
+            }
+        options = update_options_with_user_overrides(options, options_update)
 
         st_echarts(
             options=options,
