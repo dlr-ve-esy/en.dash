3f4dbce735c2e07b7d704208cf50a9fa1921b009
Josua Braun <josua.braun@dlr.de>
2023-11-10 09:32:27 +0100

    scatterplot in work

--- dashboard/tabs/tab_tab3.py
+++ dashboard/tabs/tab_tab3.py
@@ -9,31 +9,50 @@ import numpy as np
 from streamlit_extras.chart_container import chart_container
 import pandas as pd
 
+from dashboard.plots.scatterplots import scatterplot
+
 
 def create(data, metadata, plots_cfg):
     # strompreis vs. last (awardedpower)
-    data = data["SingleKey"]
-    metadata = metadata["SingleKey"]
+
+    # data = data['Line/SingleKey']
+    # metadata = metadata['Line/SingleKey']
+    data = data['Line/TwoKey']
+    metadata = metadata['Line/TwoKey']
+    print(metadata)
+
+    data = data.reset_index()
+
+
+
 
     data = data[["StoredEnergy_Austria", "StoredEnergy_Germany"]].values
 
+    
     st.subheader("test plots")
     navbar, plotarea = st.columns([0.2, 0.8])
 
     with navbar:
         st.write("this is a nav bar")
 
+        st.selectbox(
+            options=data['Region'].unique()
+        )
+
     with plotarea:
-        chart_container(pd.DataFrame(data), tabs=["Export"])
-        options = {
-            "xAxis": {},
-            "yAxis": {},
-            "series": {
-                "symbolSize": 10,
-                "data": data.tolist(),
-                "type": "scatter",
-            },
-        }
+
+        options = scatterplot(data, metadata)
+
+        # chart_container(pd.DataFrame(data), tabs=["Export"])
+        # options = {
+        #     "xAxis": {},
+        #     "yAxis": {},
+        #     "series": {
+        #         "symbolSize": 10,
+        #         "data": data.tolist(),
+        #         "type": "scatter",
+        #     },
+        # }
         user = delete_barred_user_overrides(
             plots_cfg["experimental_plot"],
             {
