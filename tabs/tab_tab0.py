cd9650fb5b26ce343278386f62b94a8e54b65141
Francesco Witte <francesco.witte@dlr.de>
2023-11-24 16:21:35 +0100

    Add example chart

--- tabs/tab_tab0.py
+++ tabs/tab_tab0.py
@@ -1,5 +1,47 @@
 import streamlit as st
+from streamlit.components import v1 as components
+from pyecharts import charts
 
 
 def create(data, metadata, cfg):
-    pass
+
+    import pyecharts.options as opts
+    from pyecharts.charts import MapGlobe
+    from pyecharts.faker import POPULATION
+
+    data = [x for _, x in POPULATION[1:]]
+    low, high = min(data), max(data)
+
+    c = (
+        MapGlobe()
+        .add_schema()
+        .add(
+            maptype="world",
+            series_name="World Population",
+            data_pair=POPULATION[1:],
+            is_map_symbol_show=False,
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .set_global_opts(
+            visualmap_opts=opts.VisualMapOpts(
+                min_=low,
+                max_=high,
+                range_text=["max", "min"],
+                is_calculable=True,
+                range_color=["lightskyblue", "yellow", "orangered"],
+            )
+        )
+        .render_embed()
+    )
+    components.html(c, height=1000)
+
+    c = charts.chart.Chart3D(
+        # init_opts=opts.InitOpts(bg_color="#000", renderer=opts.RenderOpts(is_embed_js=True))
+    )
+    c.add_globe(
+        base_texture="/app/static/data/world.topo.bathy.200401.jpg",
+        displacement_scale=0.2,
+        shading="realistic",
+        environment="/app/static/data/starfield.jpg",
+    )
+    components.html(c.render_embed(), height=1000)
