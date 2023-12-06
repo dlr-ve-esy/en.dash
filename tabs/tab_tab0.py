3257206af6ee0ce146673b85c3a078c5e01c4824
Benjamin Fuchs <Benjamin.Fuchs@dlr.de>
2023-12-06 10:50:47 +0100

    implements globe with animated 3d lines for a network in tab0

--- tabs/tab_tab0.py
+++ tabs/tab_tab0.py
@@ -1,51 +1,57 @@
 import streamlit as st
 from streamlit.components import v1 as components
 from pyecharts import charts
+import json
 
 
 def create(data, metadata, cfg):
+    c = charts.chart.Chart3D()
 
-    import pyecharts.options as opts
-    from pyecharts.charts import MapGlobe
-    from pyecharts.faker import POPULATION
+    c.options["backgroundColor"] = '#000'
+    c.options["globe"] = {
+      "baseTexture": '/app/static/data/world.topo.bathy.200401.jpg',
+      "heightTexture": '/app/static/data/world.topo.bathy.200401.jpg',
+      "shading": 'lambert',
+      "environment": '/app/static/data/starfield.jpg',
+      "light": {
+        "ambient": {
+          "intensity": 0.5
+        },
+        "main": {
+            "intensity": 0.1
+        }
+      },
+      "viewControl": {
+        "autoRotate": False
+      },
+    }
+    data = json.load(open('./data/lines.geojson', encoding="utf-8"))
 
-    data = [x for _, x in POPULATION[1:]]
-    low, high = min(data), max(data)
+    routes = [i['geometry']['coordinates']  for i in data['features']]
 
-    c = (
-        MapGlobe()
-        .add_schema()
-        .add(
-            maptype="world",
-            series_name="World Population",
-            data_pair=POPULATION[1:],
-            is_map_symbol_show=False,
-            label_opts=opts.LabelOpts(is_show=False),
-        )
-        .set_global_opts(
-            visualmap_opts=opts.VisualMapOpts(
-                min_=low,
-                max_=high,
-                range_text=["max", "min"],
-                is_calculable=True,
-                range_color=["lightskyblue", "yellow", "orangered"],
-            )
-        )
-    )
-    print(c.options["series"])
-    # c.__dict__["options"]["series"] +=
-    c.options["series"] += [{"type": "lines3D", "data": [[[0, 0], [10, 10]]], "coordinateSystem": "geo3D"}]
-    print(c.render_embed())
+    c.options["series"] = [
+      {"type": 'lines3D',
+        "coordinateSystem": 'globe',
+        "blendMode": 'lighter',
+        "lineStyle": {
+          "width": 2,
+          "color": "#3399ff",
+          "opacity": 0.5
+        },
+        "data": routes,
+        "globeIndex": 0,
+        "geo3DIndex": 0,
+        "zlevel": -10,
+        "polyline": False,
+        "silent": True,
+        "effect": {
+            "show": False,
+            "period": 4,
+            "trailWidth": 4,
+            "trailLength": 0.2,
+            "spotIntensity": 6,
+        }
+      }
+    ]
+  
     components.html(c.render_embed(), height=1000)
-
-    # c = charts.chart.Chart3D(
-    #     # init_opts=opts.InitOpts(bg_color="#000", renderer=opts.RenderOpts(is_embed_js=True))
-    # )
-    # c.add_globe(
-    #     base_texture="/app/static/data/world.topo.bathy.200401.jpg",
-    #     displacement_scale=0.2,
-    #     shading="realistic",
-    #     environment="/app/static/data/starfield.jpg",
-
-    # )
-    # components.html(c.render_embed(), height=1000)
